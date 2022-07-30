8431dbdc639ed69821cf98dc2a8c25cfc8957422
YKuklevskiy <ykuklevskiy@gmail.com>
2022-07-30 12:52:00 +0200

    Fix attribute type in vertex shader

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -1,5 +1,5 @@
 #version 330 core
-layout (location=0) in vec2 pos;
+layout (location=0) in vec3 pos;
 layout (location=1) in vec2 tex_coord;
 
 out vec2 texCoord;
@@ -10,6 +10,6 @@ uniform mat4 projection;
 
 void main()
 {
-	gl_Position = projection * view * model * vec4(pos, 0.0f, 1.0f);
+	gl_Position = projection * view * model * vec4(pos, 1.0f);
 	texCoord = tex_coord;
 }
